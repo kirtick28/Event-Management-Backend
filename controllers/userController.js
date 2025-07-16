875f1f830a858104afa12e19095a35d031ae7627
kirtick28 <kirtick.mm2023cse@sece.ac.in>
2025-07-17 02:13:49 +0530

    Update User Controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -6,10 +6,10 @@ const updateUser = async (req, res) => {
   try {
     const { id } = req.user;
     const updateData = req.body;
-    if (updateData.password) {
+    if (updateData && updateData.password !== undefined) {
       updateData.password = await bcrypt.hash(updateData.password, 10);
     }
-    if (updateData.email) {
+    if (updateData && updateData.email !== undefined) {
       const existUser = await User.findOne({ email: updateData.email });
       if (existUser)
         res.status(400).json({
@@ -17,11 +17,18 @@ const updateUser = async (req, res) => {
           message: 'Email alredy exists'
         });
     }
+    if (updateData && updateData.role) {
+      return res.status(403).json({
+        status: 'Failure',
+        message: 'Forbidden to change the role'
+      });
+    }
     const user = await User.findByIdAndUpdate(id, updateData, {
       new: true,
       runValidators: true
-    });
-    res.status(201).json({
+    }).select('name email role registeredEvents');
+
+    res.status(200).json({
       status: 'Success',
       data: {
         user
@@ -37,8 +44,13 @@ const updateUser = async (req, res) => {
 
 const getAllUsers = async (req, res) => {
   try {
-    const users = await User.find();
-    return res.status(201).json({
+    const users = await User.find({ role: 'user' })
+      .populate({
+        path: 'registeredEvents.eventId',
+        select: 'title eventDate location capacity -_id'
+      })
+      .select('name email registeredEvents');
+    return res.status(200).json({
       status: 'Success',
       data: {
         users
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -5,9 +5,8 @@ const { getAllUsers, updateUser } = require('../controllers/userController');
 const authenticate = require('../middlewares/authenticate');
 const authorize = require('../middlewares/authorize');
 
-router
-  .route('/')
-  .get(authenticate, authorize('admin'), getAllUsers)
-  .patch(authenticate, updateUser);
+router.route('/').get(authenticate, authorize('admin'), getAllUsers);
+
+router.route('/:id').put(authenticate, updateUser);
 
 module.exports = router;
